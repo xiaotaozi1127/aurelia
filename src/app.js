0efeb1177f2dafc45e863ba3e252b647afa69b84
twer <xiaotaozi1127@hotmail.com>
2016-05-15 17:26:06 +0800

    taohui add welcome, github users pages with route config.

--- src/app.js
+++ src/app.js
@@ -2,7 +2,8 @@
      configureRouter(config, router) {
          config.title = 'Aurelia';
          config.map([
-             { route: ['', 'welcome'], name: 'welcome', moduleId: './welcome', nav: true, title: 'Welcome' }
+             { route: ['', 'welcome'], name: 'welcome', moduleId: './welcome', nav: true, title: 'Welcome' },
+             { route: 'users', name: 'users', moduleId: './users', nav: true, title: 'Github Users' }
          ]);
 
          this.router = router;
